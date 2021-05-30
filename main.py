3a5d82eae71980b14f903f7e2c57d0777bc35471
AmanVirmani <aman.virmani26@gmail.com>
2021-05-30 01:05:40 -0400

    updated main file to process and save data

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import libmsi
+import numpy as np
+import random
+from pyimzml.ImzMLParser import ImzMLParser as read_msi
 
 if __name__ == "__main__":
-    file = "../data/DMSI0003_F889crd_LipidNeg_IMZML/DMSI0003_F889crd_LipidNeg_IMZML/dmsi0003scilsexporttest.imzML"
-    msi = libmsi.Imzml(file,200)
-    msi.get_peaks()
+    cph = "../real_data/DMSI0008_F895CPH_LipidNeg_IMZML/dmsi0008.imzML"
+    naive = "../real_data/DMSI0012_F888Naive_LipidNeg_IMZML/dmsi0012.imzML"
+    msi_cph = libmsi.Imzml(cph, 1)
+    msi_cph.saveImzmldata(cph.split('imzML')[0]+'npy')
+    msi_naive = libmsi.Imzml(naive, 1)
+    msi_naive.saveImzmldata(naive.split('imzML')[0]+'npy')
+    # for index in random.sample(range(len(msi.imzml_2d_array)), 10):
+    #     msi.plotSpectra(index)
+    # for mz in random.sample(range(np.shape(msi.imzml_array)[-1]), 10):
+    #     msi.plotMSI(mz)
+    # msi_cph.get_peaks()
+    # msi_naive.get_peaks()
     pass
