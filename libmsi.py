4d0a7646cf2ee35a3ca03491264554651ddbee92
AmanVirmani <aman.virmani26@gmail.com>
2021-06-19 11:12:40 -0700

    added doxygen docstrings for documentation for Imzml class

--- libmsi.py
+++ libmsi.py
@@ -1,3 +1,13 @@
+"""
+@package libmsi
+This module is designed to read and process msi data.
+
+Capabilities:
+
+1. Preprocessing: Binning and Normalization
+2. Dimensionality Reduction (PCA/ICA/NMF)
+3. Clustering (KMeans)
+"""
 from pyimzml.ImzMLParser import ImzMLParser as read_msi
 from pyimzml.ImzMLWriter import ImzMLWriter as write_msi
 from pyimzml.ImzMLParser import getionimage
@@ -12,8 +22,16 @@ from tqdm import tqdm, trange
 
 
 class Imzml:
+    """
+    @brief Imzml class is designed to load, process, and visualize MS spectra and Image data
+    """
 
     def __init__(self, filename="", bin_size=1):
+        """
+        @brief The constructor for Imzml class
+        @param filename input file to be read
+        @param bin_size bin size to be used for binning
+        """
         self.filename = filename
         self.binSize = bin_size
         if self.filename.split('.')[-1] == "imzML":
@@ -36,16 +54,31 @@ class Imzml:
         self.imzml_2d_array = self.create2dArray()
 
     def readImzml(self):
+        """
+        @brief Method to read data in .imzML format
+        """
         return read_msi(self.filename)
 
     def readImzmlArray(self):
+        """
+        @brief Method to read data in .npy format
+        """
         return np.load(self.filename, allow_pickle=True)
 
     def saveImzmldata(self, filename=None):
+        """
+        @brief Method to save processed data in .npy format
+        @param filename output filename to save the data in
+        """
         data = {'min_mz': self.min_mz, 'max_mz': self.max_mz, 'imzml_array': self.imzml_array, 'coordinates': self.coordinates}
         np.save(filename, data)
 
     def convertImzml2Array(self, min_mz, max_mz):
+        """
+        @brief Method to bin the imzml data to a 3d array format
+        @param min_mz minimum mz value to be used for binning
+        @param max_mz maximum mz value to be used for binning
+        """
         x_list = []
         min_mz -= min_mz % self.binSize
         max_mz -= max_mz % self.binSize
@@ -74,15 +107,26 @@ class Imzml:
         return X
 
     def create2dArray(self):
+        """
+        @brief Method to get a list of valid pixel spectra in 2d array format
+        """
         return self.imzml_array[~np.all(self.imzml_array == 0, axis=-1)]
 
     def reconstruct_3d_array(self, x_2d):
+        """
+        @brief Method to reconstruct the 3d image array from 2d valid pixel spectra
+        @param x_2d 2D array with valid pixel spectra
+        """
         x_3d = np.zeros([self.rows, self.cols, x_2d.shape[-1]])
         for i, (x, y, z) in enumerate(self.coordinates):
            x_3d[y-1, x-1] = x_2d[i]
         return x_3d
 
     def normalize_data(self, method='TIC'):
+        """
+        @brief Method to normalize imzml data
+        @param method the normalization method to be used.
+        """
         if method == 'TIC':
             for (x, y, z) in self.coordinates:
                 tic = sum(self.imzml_array[y-1, x-1])
@@ -91,6 +135,10 @@ class Imzml:
 
     # Dimensionality Reduction
     def performPCA(self, n_components=15):
+        """
+        @brief Method to perform Principal Component Analysis on the Imzml data
+        @param n_components number of components to keep in the Imzml data
+        """
         self.pca = PCA(n_components=n_components)
         X_train_pca = self.pca.fit_transform(self.imzml_2d_array)
         X_projected = self.pca.inverse_transform(X_train_pca)
@@ -98,6 +146,10 @@ class Imzml:
         return X_train_pca, X_projected
 
     def performICA(self, n_components=15):
+        """
+        @brief Method to perform Independent Component Analysis on the Imzml data
+        @param n_components number of components to keep in the Imzml data
+        """
         self.ica = FastICA(n_components=n_components, random_state=0)
         X_train_ica = self.ica.fit_transform(self.imzml_2d_array)
         X_projected = self.ica.inverse_transform(X_train_ica)
@@ -105,6 +157,10 @@ class Imzml:
         return X_train_ica, X_projected
 
     def performNMF(self, n_components=15):
+        """
+        @brief Method to perform Non-Negative Matrix Factorization on the Imzml data
+        @param n_components number of components to keep in the Imzml data
+        """
         self.nmf = NMF(n_components=n_components, init='random', random_state=0, max_iter=20000)
         X_train_nmf = self.nmf.fit_transform(self.imzml_2d_array)
         X_projected = self.nmf.inverse_transform(X_train_nmf)
@@ -113,6 +169,11 @@ class Imzml:
 
     # Visualization
     def plotMSI(self, mz, filename=None):
+        """
+        @brief Method to plot and save MS Image
+        @param mz m/z value for the desired MS Image
+        @param filename filename to save the plot of MS Image (optional)
+        """
         plt.figure()
         plt.imshow(self.imzml_array[:, :, mz]).set_interpolation('nearest')
         plt.colorbar()
@@ -124,6 +185,11 @@ class Imzml:
             plt.close()
 
     def plotSpectra(self, index, filename=None):
+        """
+        @brief Method to plot and save MS spectrum
+        @param index index of the desired spectra in valid pixels list
+        @param filename filename to save the plot of MS Image (optional)
+        """
         plt.plot(self.imzml_2d_array[index], 'b-')
         plt.title('MS spectra at ' + str(index) + 'index ')
         if filename is None:
@@ -133,6 +199,12 @@ class Imzml:
             plt.close()
 
     def plotTSNE(self, X_projected, n_components=2, filename=None):
+        """
+        @brief Method to plot and save TSNE
+        @param X_projected 2D Imzml data (valid pixels, no. of features)
+        @param n_components no. of components to keep
+        @param filename filename to save the plot of MS Image (optional)
+        """
         X_embedded = TSNE(n_components=2).fit_transform(X_projected)
         plt.plot(X_embedded[:, 0], X_embedded[:, 1], 'b.')
         plt.title('TSNE Visualization in 2D')
@@ -145,6 +217,12 @@ class Imzml:
         return X_embedded
 
     def plotSpectraReprojected(self, X, X_reprojected, filename=None):
+        """
+        @brief Method to plot and save the original and reprojected spectra(randomly selected 100 spectra)
+        @param X original Imzml spectral data
+        @param X_reprojected reprojected Imzml spectral data
+        @param filename filename to save the plot of MS spectra (optional)
+        """
         for i in random.sample(range(len(X_reprojected)), 100):
             f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
             ax1.plot(X[i], 'b-')
@@ -154,10 +232,17 @@ class Imzml:
             if filename is None:
                 plt.show()
             else:
-                plt.savefig(filename)
+                plt.savefig(filename.split('.')[0]+'_'+str(i)+'.png')
                 plt.close()
 
     def plotMSIReprojection(self, X_3d, X_projected_3d, mz, filename=None):
+        """
+        @brief Method to plot and save the original and reprojected images(analysis output)
+        @param X_3d original 3D Imzml data
+        @param X_projected_3d reprojected 3D Imzml data
+        @param mz m/z value to be used for comparison
+        @param filename filename to save the plot of MS Image (optional)
+        """
         f, axes = plt.subplots(1, 2, sharey=True)
         im = axes[0].imshow(X_3d[:, :, mz])  # .set_interpolation('nearest')
         axes[0].set_title('Original MSI')
@@ -173,13 +258,18 @@ class Imzml:
 
     # Peak Processing
     def get_peaks(self, plot_peaks=True, filename=None):
+        """
+        @brief Method to calculate dominant peaks in the MSI dataset
+        @param plot_peaks bool value to decide whether to plot the prominent peaks in Imzml data
+        @param filename filename to save the plot of MS peak m/z spectra (optional)
+        @return list of m/z values corresponding to prominent peaks in the Imzml data
+        """
         X = self.imzml_2d_array.copy()
         X_max = int(X.max() + 0.5)
         X_min = int(X.min())
 
         data = []
         delta = 1e-3
-        #
         th = int((X_min + X_max) / 2)
         while (True):
             if 1 - np.sum(X < th) / X.size < delta:
@@ -192,10 +282,6 @@ class Imzml:
                 break
             else:
                 th = int(new_th)
-
-        # for th in range(X_min, X_max):
-        #     if 1-np.sum(X < th)/X.size < delta:
-        #         break
 
         X[X < th] = 0
 
@@ -214,4 +300,4 @@ class Imzml:
             else:
                 plt.savefig(filename)
                 plt.close()
-        return X
+        return self.peak_mzs
