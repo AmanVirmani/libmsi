13da38e4a0abdd5cf94e405359313730d53be6d2
AmanVirmani <aman.virmani26@gmail.com>
2021-11-22 01:03:21 -0800

    updated nmf method in dataloader class
    added method to plot TSNE as well

--- dataloader.py
+++ dataloader.py
@@ -12,20 +12,28 @@ import random
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+
+from sklearnex import patch_sklearn
+patch_sklearn()
+
 from sklearn.decomposition import PCA, FastICA, NMF
+from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
+import threading
+from nonnegfac.nmf import NMF as newNMF
 
 
 class DataLoader:
     """
     @brief DataLoader class is designed to load and process mulitple msi datasets
     """
-    def __init__(self, filenames=[], split_ratio=0.75):
+    def __init__(self, filenames=[], binSize=1, split_ratio=0.75):
         """
         @brief DataLoader class constructor
         @param filenames list of filenames of the datasets
         """
         self.filenames = filenames
+        self.binSize = binSize
         self.files = self.load_files()
         self.preprocess('TIC')
         self.train_data, self.val_data = self.data_split(split_ratio)
@@ -36,8 +44,12 @@ class DataLoader:
         """
         files = []
         for file in self.filenames:
-            files.append(libmsi.Imzml(file, 1))
+            files.append(libmsi.Imzml(file, self.binSize))
         return files
+
+    def saveImzmlData(self):
+        for file in self.filenames:
+            file.saveImzmlData()
 
     def data_split(self, split_ratio=0.75):
         """
@@ -78,7 +90,7 @@ class DataLoader:
         data = np.vstack([file.imzml_2d_array for file in self.train_data])
         self.ica.fit(data)
 
-    def perform_nmf(self, n_components=15, type='all'):
+    def perform_nmf(self, n_components=15, type='all', input_data = None, n_iter=50):
         """
         @brief method to perform nmf on all datasets together
         @param n_components no. of components to keep
@@ -86,23 +98,23 @@ class DataLoader:
         @return data_list list of datasets in reduced dimensional space
         """
         data_list = []
+        self.nmf = None
         if type == 'all':
-            self.nmf = NMF(n_components=n_components, init='random', random_state=0, max_iter=20000)
-            data = np.vstack([file.imzml_2d_array for file in self.train_data])
-            self.nmf.fit(data)
-            X_transformed = self.nmf.transform(data)
+            self.nmf = newNMF()
+            data = np.vstack([file.imzml_2d_array for file in self.train_data]) if input_data is None else input_data
+            X_transformed, components, info = self.nmf.run(data, n_components,
+            max_iter = n_iter)
             start_idx = 0
             for i in range(len(self.train_data)):
-                data_list.append(X_transformed[start_idx:start_idx+ len(self.train_data[i])])
-                start_idx += len(self.train_data[i])
+                data_list.append(X_transformed[start_idx:start_idx + len(self.train_data[i].imzml_2d_array)])
+                start_idx += len(self.train_data[i].imzml_2d_array)
         elif type == 'each':
             for file in self.files:
                 X_projected, _ = file.performNMF(n_components=n_components)
                 data_list.append(X_projected)
-        return data_list
+        return {'nmf_data_list': data_list, 'nmf_component_spectra':components}
 
-
-    def perform_kmeans(self, data_list, n_clusters = 3):
+    def perform_kmeans(self, data_list, n_clusters=3):
         """
         @brief method to perform k means clustering
         @param data_list list of data sets to cluster
@@ -122,17 +134,41 @@ class DataLoader:
         @return cluster_lists list of cluster labels and centroids for each dataset
         """
         n_means = len(cluster_lists[0]['clusters'])
-        data =np.vstack([cluster_lists[i]['clusters'] for i in range(len(cluster_lists))])
+        data = np.vstack([cluster_lists[i]['clusters'] for i in range(len(cluster_lists))])
         km = KMeans(n_clusters=n_means, random_state=0).fit(data)
         for i in range(len(cluster_lists)):
             # first fix the cluster means
             clusters = np.empty_like(cluster_lists[i]['clusters'])
             labels = cluster_lists[i]['labels'].copy()
             for j in range(n_means):
-                clusters[km.labels_[n_means*i+j]] = cluster_lists[i]['clusters'][j]
-                cluster_lists[i]['labels'][labels == j] = km.labels_[n_means*i+j]
+                clusters[km.labels_[n_means * i + j]] = cluster_lists[i]['clusters'][j]
+                cluster_lists[i]['labels'][labels == j] = km.labels_[n_means * i + j]
             cluster_lists[i]['clusters'] = clusters
         return cluster_lists
+
+    def plot_tsne(self, data, labels, filename=None):
+        """
+        @brief Method to plot and save TSNE
+        @param X_projected 2D Imzml data (valid pixels, no. of features)
+        @param n_components no. of components to keep
+        @param filename filename to save the plot of MS Image (optional)
+        """
+        tsne = TSNE(n_components=2, init='pca', n_jobs=-1)
+        X_embedded = tsne.fit_transform(data)
+        np.save('tsne_nmf_25_2_objects.npy', tsne)
+        print('KL divergenc: ', tsne.kl_divergence_)
+        red = labels == 1
+        green = labels == 0
+        plt.scatter(X_embedded[red, 0], X_embedded[red, 1], c="r")
+        plt.scatter(X_embedded[green, 0], X_embedded[green, 1], c="g")
+        plt.title('TSNE Visualization in 2D')
+        # can choose to comment this and do: plt.savefig(filename)
+        if filename is None:
+            plt.show()
+        else:
+            plt.savefig(filename)
+            plt.close()
+
 
 
     def plot_clusters(self, data_list, cluster_list, fn_prefix='cph_'):
@@ -147,16 +183,16 @@ class DataLoader:
             fig = plt.figure(figsize=(8, 8))
             plt.scatter(data[:, 0], data[:, 1], c=cluster_dict['labels'], cmap=matplotlib.colors.ListedColormap(colors))
             cb = plt.colorbar()
-            loc = np.arange(0, max(cluster_dict['labels']), max(cluster_dict['labels'])/float(len(colors)))
+            loc = np.arange(0, max(cluster_dict['labels']), max(cluster_dict['labels']) / float(len(colors)))
             cb.set_ticks(loc)
             cb.set_ticklabels(colors)
             plt.xlim(0, 0.1)
             plt.ylim(0, 0.1)
-            plt.savefig(fn_prefix+'cluster_plot_'+str(i)+'.png')
+            plt.savefig(fn_prefix + 'cluster_plot_' + str(i) + '.png')
             # plt.show()
             pass
 
-    def plot_cluster_reprojection(self, cluster_list, fn_prefix='cph_'):
+    def plot_cluster_reprojection(self, cluster_list, type='each', fn_prefix='cph_'):
         """
         @brief method to plot the reprojection images of the clusters in reduced dimesions
         @param cluster_list list of cluster labels and centroids for each dataset
@@ -184,41 +220,47 @@ class DataLoader:
 
             im = axes[0, 0].imshow(im_frame, cmap=cmap)
             cb = fig.colorbar(im, ax=axes[0, 0], spacing='proportional', ticks=bounds)
-            cluster_mean_spectra = cluster_dict['clusters'].dot(self.files[i].nmf.components_)
+
+            if type == 'each':
+                cluster_mean_spectra = cluster_dict['clusters'].dot(self.files[i].nmf.components_)
+            else:
+                cluster_mean_spectra = cluster_dict['clusters'].dot(self.nmf.components_)
+
             axes[0, 1].plot(cluster_mean_spectra[0], 'b-')
             axes[1, 0].plot(cluster_mean_spectra[1], 'g-')
             axes[1, 1].plot(cluster_mean_spectra[2], 'r-')
-            plt.savefig(fn_prefix+'cluster_reprojection_' + str(i) + '.png')
+            plt.savefig(fn_prefix + 'cluster_reprojection_' + str(i) + '.png')
 
-if __name__=='__main__':
-   cph_filenames = ['./dmsi0008.npy', './dmsi0011.npy']
-   naive_filenames = ['./dmsi0009.npy', './dmsi0012.npy']
-   cph_loader = DataLoader(cph_filenames, 1)
-   naive_loader = DataLoader(naive_filenames, 1)
 
-   cph_data_list = cph_loader.perform_nmf(n_components=2, type='all')
-   cph_cluster_list = cph_loader.perform_kmeans(cph_data_list, 3)
-   cph_cluster_list = cph_loader.align_clusters(cph_cluster_list)
+if __name__ == '__main__':
+    cph_filenames = ['./dmsi0008.npy', './dmsi0011.npy']
+    naive_filenames = ['./dmsi0009.npy', './dmsi0012.npy']
+    cph_loader = DataLoader(cph_filenames, 1)
+    naive_loader = DataLoader(naive_filenames, 1)
 
-   cph_loader.plot_clusters(cph_data_list, cph_cluster_list, 'cph_2')
-   cph_loader.plot_cluster_reprojection(cph_cluster_list, 'cph_test_')
-   cluster_mean_spectra = cph_cluster_list[0]['clusters'].dot(cph_loader.files[0].nmf.components_)
-   plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
-   plt.savefig('cph_mean_spectra_0.jpg')
-   cluster_mean_spectra = cph_cluster_list[1]['clusters'].dot(cph_loader.files[1].nmf.components_)
-   plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
-   plt.savefig('cph_mean_spectra_1.jpg')
+    cph_data_list = cph_loader.perform_nmf(n_components=2, type='all')
+    cph_cluster_list = cph_loader.perform_kmeans(cph_data_list, 3)
+    cph_cluster_list = cph_loader.align_clusters(cph_cluster_list)
 
-   naive_data_list = naive_loader.perform_nmf(n_components=2, type='each')
-   naive_cluster_list = naive_loader.perform_kmeans(naive_data_list, 3)
-   # # naive_loader.plot_clusters(naive_data_list, naive_labels_list, 'naive_20')
-   # naive_loader.plot_cluster_reprojection(naive_labels_list, 'naive_')
-   cluster_mean_spectra = naive_cluster_list[0]['clusters'].dot(naive_loader.files[0].nmf.components_)
-   plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
-   plt.savefig('naive_mean_spectra_0.jpg')
-   cluster_mean_spectra = naive_cluster_list[1]['clusters'].dot(naive_loader.files[1].nmf.components_)
-   plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
-   plt.savefig('naive_mean_spectra_1.jpg')
+    cph_loader.plot_clusters(cph_data_list, cph_cluster_list, 'cph_2')
+    cph_loader.plot_cluster_reprojection(cph_cluster_list, 'cph_test_')
+    cluster_mean_spectra = cph_cluster_list[0]['clusters'].dot(cph_loader.files[0].nmf.components_)
+    plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
+    plt.savefig('cph_mean_spectra_0.jpg')
+    cluster_mean_spectra = cph_cluster_list[1]['clusters'].dot(cph_loader.files[1].nmf.components_)
+    plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
+    plt.savefig('cph_mean_spectra_1.jpg')
 
-   naive_loader.files[0].performTSNE(naive_data_list[0], filename='TSNE_naive_0.jpg')
-   pass
+    naive_data_list = naive_loader.perform_nmf(n_components=2, type='each')
+    naive_cluster_list = naive_loader.perform_kmeans(naive_data_list, 3)
+    # # naive_loader.plot_clusters(naive_data_list, naive_labels_list, 'naive_20')
+    # naive_loader.plot_cluster_reprojection(naive_labels_list, 'naive_')
+    cluster_mean_spectra = naive_cluster_list[0]['clusters'].dot(naive_loader.files[0].nmf.components_)
+    plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
+    plt.savefig('naive_mean_spectra_0.jpg')
+    cluster_mean_spectra = naive_cluster_list[1]['clusters'].dot(naive_loader.files[1].nmf.components_)
+    plt.plot(cluster_mean_spectra[0], cluster_mean_spectra[1], 'b.')
+    plt.savefig('naive_mean_spectra_1.jpg')
+
+    naive_loader.files[0].performTSNE(naive_data_list[0], filename='TSNE_naive_0.jpg')
+    pass
